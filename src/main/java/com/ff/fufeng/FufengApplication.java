0a406fbcd8796e727299e554aa07012aefa98807
ff <bhysrdys@126.com>
2018-07-11 09:57:27 +0800

    Initial commit

---
+++ src/main/java/com/ff/fufeng/FufengApplication.java
@@ -0,0 +1,12 @@
+package com.ff.fufeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FufengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FufengApplication.class, args);
+	}
+}
